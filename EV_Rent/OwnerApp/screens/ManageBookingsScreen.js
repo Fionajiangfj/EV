110926c434241153df1243ff9e1b9f7cebb7811d
Fionajiangfj <67009589+Fionajiangfj@users.noreply.github.com>
2024-03-30 19:21:33 -0400

    login and navigation for owner app done

---
+++ EV_Rent/OwnerApp/screens/ManageBookingsScreen.js
@@ -0,0 +1,9 @@
+import { Text } from "react-native";
+
+const ManageBookingsScreen = () => {
+    return (
+        <Text>Manage Bookings Screen</Text>
+    )
+}
+
+export default ManageBookingsScreen;
