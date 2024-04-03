4ecb08381dc2f78b6457066e2a36b4591791a548
Pabitapun23 <pabita.pun23@gmail.com>
2024-04-02 23:28:07 -0400

    myRentalsScreen added with detailScreen and components

--- EV_Rent/OwnerApp/App.js
+++ EV_Rent/OwnerApp/App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
 
 // screens
 import RentalFromScreen from './screens/RentalFormScreen';
@@ -10,8 +11,17 @@ import MyRentalListScreen from './screens/MyRentalListScreen';
 
 // icons
 import { MaterialIcons } from '@expo/vector-icons';
+import MyRentalDetailsScreen from './screens/MyRentalDetailsScreen';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const RentalListStack = () => (
+  <Stack.Navigator initialRouteName='My Rental List' >
+    <Stack.Screen name="My Rental List" component={MyRentalListScreen} options={{ headerShown: false }}/>
+    <Stack.Screen name="Rental Details" component={MyRentalDetailsScreen} />
+  </Stack.Navigator>
+);
 
 export default function App() {
   return (
@@ -22,16 +32,21 @@ export default function App() {
             if (route.name == "Rental Form") {              
               return <MaterialIcons name="person-search" size={24} color={color} />
             }
-            if (route.name === "Rental List") {              
+            if (route.name === "My Rental List") {              
               return <MaterialIcons name="format-list-bulleted" size={24} color={color} />
             }
           },
-          tabBarActiveTintColor: 'tomato',
+          tabBarActiveTintColor: '#0064B1',
           tabBarInactiveTintColor: 'gray',
+          headerStyle: {
+            backgroundColor: "#0064B1"
+          },
+          headerTintColor: "#fff"
         })}
       > 
         <Tab.Screen name="Rental Form" component={RentalFromScreen} />
-        <Tab.Screen name="Rental List" component={MyRentalListScreen} />
+        <Tab.Screen name="My Rental List" component={RentalListStack} />
+
       </Tab.Navigator>
     </NavigationContainer>
   );
